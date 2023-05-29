9679b6b3cb433095495633dd69c6e02f36852cb2
Raymond Zhong <rzhong669@gmail.com>
2023-05-29 12:13:25 -0500

    Refactor displayController to prevent readding duplicate event listeners

--- script.js
+++ script.js
@@ -134,13 +134,16 @@ const displayController = (() => {
     let counter = 0; // 0 -> 8
     for (let row = 0; row < 3; row++) {
       for (let col = 0; col < 3; col++) {
-        markerButtons[counter].disabled = false;
         //This is running multiple times causing the event listeners to duplicate and returning errors whenver a new game is started.
-        markerButtons[counter].addEventListener("click", (event) => {
-          gameLogic.placeMarker(row, col);
-          event.target.innerText = gameboard.getArr()[row][col];
-          event.target.disabled = true;
-        });
+        markerButtons[counter].addEventListener(
+          "click",
+          (event) => {
+            gameLogic.placeMarker(row, col);
+            event.target.innerText = gameboard.getArr()[row][col];
+            event.target.disabled = true;
+          },
+          { once: true }
+        );
         counter++;
       }
     }
@@ -173,9 +176,10 @@ const displayController = (() => {
 
   const reset = () => {
     const markerButtons = document.querySelectorAll(".marker-btn");
-    markerButtons.forEach((button) => (button.innerText = ""));
+    markerButtons.forEach((button) => {
+      button.innerText = "";
+      button.disabled = false;
+    });
   };
   return { initGame, showResult };
 })();
-
-// displayController.initGame();
